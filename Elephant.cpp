c66f8b3924f8c5c23bb01c00a9071b43fc8d998c
root <rihards.belinskis@gmail.com>
2020-04-07 13:09:47 +0200

    Elephant.cpp

---
+++ Elephant.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int x;		// number of units his friend is away from initial pos.
+
+	cin >> x;
+	cout << (x + 4) / 5 << endl;
+	return 0;
+}
+
