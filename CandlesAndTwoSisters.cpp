46458efdfa0812501e87f595940a1ce944ba5997
root <rihards.belinskis@gmail.com>
2020-04-22 18:41:56 +0200

    Candies and Two Sisters

---
+++ CandlesAndTwoSisters.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n, ans = 0;
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		ans = (n-1) / 2;
+		cout << ans << endl;
+	}
+	return 0;
+}
